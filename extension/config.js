413ed45c40367bc1ba51d7f35846cf1e02648c40
Galoremw <Galoremw@users.noreply.github.com>
2026-09-22 02:35:06 +0800

    point extension at hosted api

--- extension/config.js
+++ extension/config.js
@@ -1,3 +1,3 @@
 // Set this to the public FastAPI origin used by the hosted dashboard.
 // Example: https://monitor-api.example.com
-globalThis.ALIEXPRESS_MONITOR_API_BASE = "http://127.0.0.1:8000";
+globalThis.ALIEXPRESS_MONITOR_API_BASE = "https://aliexpress-monitor-api.onrender.com";
